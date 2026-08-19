559e73c86f29992c63940be36fd217d30eceb19c
Sean Du <do4suki@gmail.com>
2026-08-20 06:19:13 +0800

    gms: Always rebuild split packages
    This prevents stale generated files from surviving package updates.
    
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 # Shebang is intentionally missing - do not run as a script
 
-[ -f vendor/google/gms/proprietary/product/apex/com.google.android.gmssystem.prodvic.apex ] || cat vendor/google/gms/proprietary/product/apex/com.google.android.gmssystem.prodvic.apex.part* > vendor/google/gms/proprietary/product/apex/com.google.android.gmssystem.prodvic.apex
-[ -f vendor/google/gms/proprietary/product/app/Gmail2/Gmail2.apk ] || cat vendor/google/gms/proprietary/product/app/Gmail2/Gmail2.apk.part* > vendor/google/gms/proprietary/product/app/Gmail2/Gmail2.apk
-[ -f vendor/google/gms/proprietary/product/app/Maps/Maps.apk ] || cat vendor/google/gms/proprietary/product/app/Maps/Maps.apk.part* > vendor/google/gms/proprietary/product/app/Maps/Maps.apk
-[ -f vendor/google/gms/proprietary/product/app/Photos/Photos.apk ] || cat vendor/google/gms/proprietary/product/app/Photos/Photos.apk.part* > vendor/google/gms/proprietary/product/app/Photos/Photos.apk
-[ -f vendor/google/gms/proprietary/product/app/TrichromeLibrary64/TrichromeLibrary64.apk ] || cat vendor/google/gms/proprietary/product/app/TrichromeLibrary64/TrichromeLibrary64.apk.part* > vendor/google/gms/proprietary/product/app/TrichromeLibrary64/TrichromeLibrary64.apk
-[ -f vendor/google/gms/proprietary/product/app/YouTube/YouTube.apk ] || cat vendor/google/gms/proprietary/product/app/YouTube/YouTube.apk.part* > vendor/google/gms/proprietary/product/app/YouTube/YouTube.apk
-[ -f vendor/google/gms/proprietary/product/priv-app/Messages/Messages.apk ] || cat vendor/google/gms/proprietary/product/priv-app/Messages/Messages.apk.part* > vendor/google/gms/proprietary/product/priv-app/Messages/Messages.apk
-[ -f vendor/google/gms/proprietary/product/priv-app/Phonesky/Phonesky.apk ] || cat vendor/google/gms/proprietary/product/priv-app/Phonesky/Phonesky.apk.part* > vendor/google/gms/proprietary/product/priv-app/Phonesky/Phonesky.apk
-[ -f vendor/google/gms/proprietary/product/priv-app/Velvet/Velvet.apk ] || cat vendor/google/gms/proprietary/product/priv-app/Velvet/Velvet.apk.part* > vendor/google/gms/proprietary/product/priv-app/Velvet/Velvet.apk
+cat vendor/google/gms/proprietary/product/apex/com.google.android.gmssystem.prodvic.apex.part* > vendor/google/gms/proprietary/product/apex/com.google.android.gmssystem.prodvic.apex
+cat vendor/google/gms/proprietary/product/app/Gmail2/Gmail2.apk.part* > vendor/google/gms/proprietary/product/app/Gmail2/Gmail2.apk
+cat vendor/google/gms/proprietary/product/app/Maps/Maps.apk.part* > vendor/google/gms/proprietary/product/app/Maps/Maps.apk
+cat vendor/google/gms/proprietary/product/app/Photos/Photos.apk.part* > vendor/google/gms/proprietary/product/app/Photos/Photos.apk
+cat vendor/google/gms/proprietary/product/app/TrichromeLibrary64/TrichromeLibrary64.apk.part* > vendor/google/gms/proprietary/product/app/TrichromeLibrary64/TrichromeLibrary64.apk
+cat vendor/google/gms/proprietary/product/app/YouTube/YouTube.apk.part* > vendor/google/gms/proprietary/product/app/YouTube/YouTube.apk
+cat vendor/google/gms/proprietary/product/priv-app/Messages/Messages.apk.part* > vendor/google/gms/proprietary/product/priv-app/Messages/Messages.apk
+cat vendor/google/gms/proprietary/product/priv-app/Phonesky/Phonesky.apk.part* > vendor/google/gms/proprietary/product/priv-app/Phonesky/Phonesky.apk
+cat vendor/google/gms/proprietary/product/priv-app/Velvet/Velvet.apk.part* > vendor/google/gms/proprietary/product/priv-app/Velvet/Velvet.apk
